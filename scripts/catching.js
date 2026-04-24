5017400752717ab8c078ce40bf1bce4180687731
Sass <seri.lee.ac@gmail.com>
2026-04-24 13:22:43 -0300

    Creature Catching base

---
+++ scripts/catching.js
@@ -0,0 +1,11 @@
+// Randomise creature positioning
+document.querySelectorAll(".creature").forEach((creature) => {
+  var directions = ["+", "-"];
+  var xDir = directions[Math.floor(Math.random() * directions.length)];
+  var yDir = directions[Math.floor(Math.random() * directions.length)];
+  var xMod = Math.floor(Math.random() * 100);
+  var yMod = Math.floor(Math.random() * 100);
+
+  creature.style.marginLeft = xDir + xMod + "px";
+  creature.style.marginTop = yDir + yMod + "px";
+});
