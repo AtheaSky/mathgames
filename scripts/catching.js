015c94714ff0a05e1bac807b1fb665f9f33c10c6
Sass <seri.lee.ac@gmail.com>
2026-05-29 15:39:00 -0300

    Optional catching timer

--- scripts/catching.js
+++ scripts/catching.js
@@ -46,6 +46,13 @@ function start() {
     answers = qa[1];
   }
   console.log(qa);
+
+  // Get timer
+  let timerStart = +document.getElementById("timerSetting").value;
+  if (timerStart != 0) {
+    var totalSec = timerStart * 60;
+    timer(Math.floor(timerStart), (timerStart - Math.floor(timerStart)) * 60);
+  }
 }
 
 // Randomise creature positioning
@@ -64,6 +71,46 @@ document.querySelectorAll(".creature").forEach((creature) => {
 var creaturesLeft = iniCreatureAmt;
 var creaturesCaught = 0;
 let currentQuestion;
+var timeUp = false;
+
+// If enabled, start timer
+function timer(startingMins, startingSecs) {
+  document.getElementById("timer").style = "visible";
+  var min = startingMins;
+  var sec = startingSecs;
+
+  var timer = setInterval(function () {
+    // If seconds under 10, add leading 0
+    var secStr = sec;
+    if (sec < 10) {
+      secStr = `0${sec}`;
+    }
+
+    // Show current timer
+    document.getElementById("timer").innerHTML = `${min}:${secStr}`;
+    sec--;
+
+    // When seconds reach 0
+    if (sec < 0) {
+      // If minutes left, decrement
+      if (min > 0) {
+        min -= 1;
+        sec = 59;
+
+        // If 1 minute left, turn yellow
+        if (min == 0) {
+          document.getElementById("timer").style = "color: #413212";
+        }
+      } else {
+        // Else, end
+        clearInterval(timer);
+        timeUp = true;
+        document.getElementById("timer").style = "color: #6e1b1b";
+        endGame();
+      }
+    }
+  }, 1000);
+}
 
 // When roaming creature clicked
 function catchAttempt(creatureId) {
@@ -106,24 +153,35 @@ function catchResult() {
 
   // If no fish left, end
   if (creaturesLeft == 0) {
-    var endTitle;
-    var endBody;
-    // Congratulate based on fish caught
-    if (creaturesCaught == iniCreatureAmt) {
-      endTitle = "Congratulations!";
-      endBody = "You caught all of the fish!";
-    } else if (creaturesCaught > 0) {
-      endTitle = "Good job!";
-      endBody = `You caught ${creaturesCaught} out of ${iniCreatureAmt} fish!`;
-    } else if (creaturesCaught == 0) {
-      endTitle = "No more fish!";
-      endBody = "All of the fish swam away. Try again!";
-    }
-    // If time ran out, change title to "Time up!" instead
-    //STUB
+    endGame();
+  }
+}
 
-    // Insert text to end modal
-    const endContent = `<h2>${endTitle}</h2><br /> \
+function endGame() {
+  var endTitle;
+  var endBody;
+
+  // Freeze timer
+  clearInterval(timer);
+
+  // Congratulate based on fish caught
+  if (creaturesCaught == iniCreatureAmt) {
+    endTitle = "Congratulations!";
+    endBody = "You caught all of the fish!";
+  } else if (creaturesCaught > 0) {
+    endTitle = "Good job!";
+    endBody = `You caught ${creaturesCaught} out of ${iniCreatureAmt} fish!`;
+  } else if (creaturesCaught == 0) {
+    endTitle = "No more fish!";
+    endBody = "All of the fish swam away. Try again!";
+  }
+  // If time ran out, change title to "Time up!" instead
+  if (timeUp) {
+    endTitle = "Time up!";
+  }
+
+  // Insert text to end modal
+  const endContent = `<h2>${endTitle}</h2><br /> \
         <p>${endBody}</p><br /> \
         <a href="./index.html" class="modalBtn btn" id="homeBtn">Home</a> \
         <span \
@@ -132,35 +190,34 @@ function catchResult() {
         id="replayBtn"> \
         Replay \
         </span>`;
-    document.getElementById("endModalContent").innerHTML += endContent;
+  document.getElementById("endModalContent").innerHTML += endContent;
 
-    document.getElementById("endModal").style.display = "block";
+  document.getElementById("endModal").style.display = "block";
 
-    // Trigger confetti based on caught fish
-    const emojiConfetti = new JSConfetti();
+  // Trigger confetti based on caught fish
+  const emojiConfetti = new JSConfetti();
 
-    emojiConfetti.addConfetti({
-      emojis: ["🐟", "🐠"],
-      emojiSize: 40,
-      confettiNumber: Math.round(20 * (creaturesCaught / iniCreatureAmt)),
+  emojiConfetti.addConfetti({
+    emojis: ["🐟", "🐠"],
+    emojiSize: 40,
+    confettiNumber: Math.round(20 * (creaturesCaught / iniCreatureAmt)),
+  });
+
+  // If all caught, add some regular confetti
+  if (creaturesCaught == iniCreatureAmt) {
+    const jsConfetti = new JSConfetti();
+
+    // Trigger colorful confetti
+    jsConfetti.addConfetti({
+      confettiColors: [
+        "#ed795f",
+        "#fca4b6",
+        "#35b297",
+        "#6dd2e7",
+        "#fc69c5",
+        "#f14f55",
+      ],
+      confettiNumber: 200,
     });
-
-    // If all caught, add some regular confetti
-    if (creaturesCaught == iniCreatureAmt) {
-      const jsConfetti = new JSConfetti();
-
-      // Trigger colorful confetti
-      jsConfetti.addConfetti({
-        confettiColors: [
-          "#ed795f",
-          "#fca4b6",
-          "#35b297",
-          "#6dd2e7",
-          "#fc69c5",
-          "#f14f55",
-        ],
-        confettiNumber: 200,
-      });
-    }
   }
 }
